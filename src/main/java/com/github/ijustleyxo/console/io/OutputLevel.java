e74b8df3427308a7da5133e0bf4273d8779a9193
Leonard Pfeiffer <leonard.s.pfeiffer@gmx.net>
2025-01-06 17:35:38 +0100

    Rename Console Output Interfaces (#5)

--- src/main/java/com/github/ijustleyxo/console/io/OutputLevel.java
+++ src/main/java/com/github/ijustleyxo/console/io/OutputLevel.java
@@ -3,7 +3,7 @@ package com.github.ijustleyxo.console.io;
 /**
  * Interface for logging detail level management classes
  */
-public interface Level {
+public interface OutputLevel {
     /**
      * Returns the reference value of the detail level, used to compare detail levels. Larger numbers correspond to more detail whilst small numbers correspond to less detail. Default reference value is {@code 0}.
      * @return The reference value of the detail level
